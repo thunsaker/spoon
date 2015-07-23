870b9adbc73e5c0a51c6e8f80732a147719bde75
Thomas Hunsaker <thomas.hunsaker@gmail.com>
2015-07-23 16:40:29 -0700

    Wired up deps and fixed gulp issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,43 @@
-var gulp = require('gulp');
-var jade = require('gulp-jade');
-var del = require('del');
+var gulp = require('gulp'),
+    jade = require('gulp-jade'),
+    del = require('del'),
+    livereload = require('gulp-livereload');
 
-gulp.task('default', ['clean','render']);
-
-gulp.task('clean', function() {
-    del(['dist/*'])
+gulp.task('default', ['clean'], function() {
+    gulp.start('jade','copy');
 });
 
-gulp.task('render', function() {
+gulp.task('clean', function(cb) {
+    del(['dist/*'], cb)
+});
+
+gulp.task('jade', function() {
     gulp.src('*.jade')
         .pipe(jade())
-        .pipe(gulp.dest('./dist/'))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('copy', function() {
+    // css
+    gulp.src([
+            'bower_components/pebble-slate/dist/css/slate.min.css'
+        ])
+        .pipe(gulp.dest('./dist/css'));
+    // js
+    gulp.src([
+            'bower_components/pebble-slate/dist/js/slate.min.js'
+        ])
+        .pipe(gulp.dest('./dist/js'));
+
+    // img
+    gulp.src([
+            'img/*.png'
+        ])
+        .pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('*.jade', ['jade']);
+    livereload.listen();
+    gulp.watch(['.dist/**']).on('change', livereload.changed);
 });

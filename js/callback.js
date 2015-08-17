7de5b87c96c2d5331efbf419cfd54f6563dc2c03
Thomas Hunsaker <d4rthsid@gmail.com>
2015-08-17 19:59:11 +0000

    Update 2015-08-17T19:59:10.111Z

--- js/callback.js
+++ js/callback.js
@@ -1,7 +1,4 @@
 var result = getToken();
-alert("Result = " + result + " token = " + result.token);
 var redirectUrl = "pebblejs://close"; // auto-close
 redirectUrl += "#" + encodeURIComponent(JSON.stringify(result.token));
-alert(redirectUrl);
-
 document.location = redirectUrl;
--- js/token.js
+++ js/token.js
@@ -1,10 +1,7 @@
 function getToken() {
     var q = window.location.hash.toString();
-    alert("Hash " + q);
-    alert("Urls \n window.location.href=" + window.location.href + " window.location=" + window.location + " document.URL=" + document.URL);
     if(q !== null && q.length > 0) {
         var myToken = q.substring(14);
-        alert("Token = " + myToken);
         var result = {};
         if(myToken.length > 0) {
             result.result = true;
@@ -13,7 +10,6 @@ function getToken() {
             result.result = false;
             result.token = "";
         }
-        alert("Result = " + result);
         return result;
     } else {
         return null;

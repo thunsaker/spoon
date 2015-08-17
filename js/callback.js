03e2af53be3e9208eb874934d7726ad587739a1b
Thomas Hunsaker <d4rthsid@gmail.com>
2015-08-17 18:15:00 +0000

    Update 2015-08-17T18:15:00.038Z

--- js/callback.js
+++ js/callback.js
@@ -1,17 +1,7 @@
 $(document).ready(function() {
     var result = getToken();
-    // var redirectUrl = "config.html"; // dev
-    // var redirectUrl = "https://thunsaker.github.io/spoon/config.html"; // prod
-    // window.location.href = redirectUrl + "#access_token=" + result.token;
     
     var redirectUrl = "pebblejs://close"; // auto-close
     
-    var config = {};
-    var token = {};
-    config.token = result;
-    config.theme = 0;
-    config.unit = 0;
-
-    // window.location.href = redirectUrl + "#" + JSON.stringify(config);
-    window.location.href = redirectUrl + "#" + encodeURIComponent(JSON.stringify(result.token));
+    document.location = redirectUrl + "#" + encodeURIComponent(JSON.stringify(result.token));
 });

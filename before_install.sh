26bc3aad65a88978dd7cf431ec4009a657e029ed
Thomas Hunsaker <thunsaker@users.noreply.github.com>
2016-01-20 13:21:19 -0700

    Forgot the switch to bz2

--- before_install.sh
+++ before_install.sh
@@ -12,11 +12,11 @@ PEBBLE_SDK_VER=${PEBBLE_SDK#PebbleSDK-}
 if [ ! -d $HOME/pebble-dev/${PEBBLE_SDK} ]; then
   # Legacy SDK path before 3.8
   # wget https://sdk.getpebble.com/download/${PEBBLE_SDK_VER} -O PebbleSDK-${PEBBLE_SDK_VER}.tar.gz
-  wget https://s3.amazonaws.com/assets.getpebble.com/sdk3/release/sdk-core-${PEBBLE_SDK_VER}.tar.bz2 -O PebbleSDK-${PEBBLE_SDK_VER}.tar.gz
+  wget https://s3.amazonaws.com/assets.getpebble.com/sdk3/release/sdk-core-${PEBBLE_SDK_VER}.tar.bz2 -O PebbleSDK-${PEBBLE_SDK_VER}.tar.bz2
   wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
 
   # Extract the SDK
-  tar zxf PebbleSDK-${PEBBLE_SDK_VER}.tar.gz -C ~/pebble-dev/
+  tar xvjf PebbleSDK-${PEBBLE_SDK_VER}.tar.bz2 -C ~/pebble-dev/
   # Extract the toolchain
   tar zxf arm-cs-tools-ubuntu-universal.tar.gz -C ~/pebble-dev/${PEBBLE_SDK}
 

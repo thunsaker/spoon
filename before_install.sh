e91ad2621cdc10f2f40c16b47c2a7332479c6c31
Thomas Hunsaker <thunsaker@users.noreply.github.com>
2016-01-20 12:59:23 -0700

    Attempting fix of SDK download path

--- before_install.sh
+++ before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-echo 'pBuild 1.0'
+echo 'pBuild 1.1'
 echo 'Installing Pebble SDK and its Dependencies...'
 
 cd ~
@@ -10,7 +10,9 @@ touch ~/pebble-dev/ENABLE_ANALYTICS
 # Get the Pebble SDK and toolchain
 PEBBLE_SDK_VER=${PEBBLE_SDK#PebbleSDK-}
 if [ ! -d $HOME/pebble-dev/${PEBBLE_SDK} ]; then
-  wget https://sdk.getpebble.com/download/${PEBBLE_SDK_VER} -O PebbleSDK-${PEBBLE_SDK_VER}.tar.gz
+  # Legacy SDK path before 3.8
+  # wget https://sdk.getpebble.com/download/${PEBBLE_SDK_VER} -O PebbleSDK-${PEBBLE_SDK_VER}.tar.gz
+  wget https://s3.amazonaws.com/assets.getpebble.com/sdk3/release/sdk-core-${PEBBLE_SDK_VER}.tar.bz2 -O PebbleSDK-${PEBBLE_SDK_VER}.tar.gz
   wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
 
   # Extract the SDK

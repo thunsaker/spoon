a4cfc1dfd58c8ede4fd8ad7bf75930fd3b509c41
Thomas Hunsaker <d4rthsid@gmail.com>
2016-01-20 22:27:11 +0000

    Removing getToken

--- js/callback.js
+++ js/callback.js
@@ -1,4 +1,5 @@
-var result = getToken();
+// var result = getToken();
 var redirectUrl = "pebblejs://close"; // auto-close
-redirectUrl += "#" + encodeURIComponent(JSON.stringify(result.token));
+// redirectUrl += "#" + encodeURIComponent(JSON.stringify(result.token));
+redirectUrl += "#" + encodeURIComponent(window.location.hash.toString());
 document.location = redirectUrl;

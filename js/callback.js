76c77bd75d85784cb2fedb8378a1e8adacb626b5
Thomas Hunsaker <d4rthsid@gmail.com>
2016-01-20 22:12:35 +0000

    Update 2016-01-20T22:12:34.603Z

--- js/callback.js
+++ js/callback.js
@@ -1,4 +1,5 @@
-var result = getToken();
-var redirectUrl = "pebblejs://close#"; // auto-close
-redirectUrl += "#" + encodeURIComponent(JSON.stringify(result.token));
+// var result = getToken();
+var redirectUrl = "pebblejs://close"; // auto-close
+// redirectUrl += "#" + encodeURIComponent(JSON.stringify(result.token));
+redirectUrl += window.location.hash.toString();
 document.location = redirectUrl;

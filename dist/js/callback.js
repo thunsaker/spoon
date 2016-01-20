328116b2b38e9945cd850b402146ee644d03e74e
Thomas Hunsaker <d4rthsid@gmail.com>
2016-01-20 19:18:42 +0000

    Reverted callback url to handle older apps

--- dist/js/callback.js
+++ dist/js/callback.js
@@ -1,4 +1,4 @@
 var result = getToken();
-var redirectUrl = "pebblejs://close"; // auto-close
+var redirectUrl = "pebblejs://close#"; // auto-close
 redirectUrl += "#" + encodeURIComponent(JSON.stringify(result.token));
 document.location = redirectUrl;
